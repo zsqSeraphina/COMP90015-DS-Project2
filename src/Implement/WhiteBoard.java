fe8fdff21b98b52454e32713e385d8e6f0071b32
zsqSeraphina <zsqseraphina@outlook.com>
2022-05-18 20:08:10 +0800

    add initial files

---
+++ src/Implement/WhiteBoard.java
@@ -0,0 +1,10 @@
+package src.Implement;
+
+import src.Interface.IWhiteBoard;
+
+import java.rmi.RemoteException;
+
+public class WhiteBoard {
+
+    private boolean isManager;
+}
---
+++ src/Interface/IWhiteBoard.java
@@ -0,0 +1,14 @@
+package src.Interface;
+
+import java.rmi.Remote;
+
+/**
+ * Siqi Zhou
+ * 903274
+ */
+public interface IWhiteBoard extends Remote{
+
+
+
+    public void initialise() throws java.rmi.RemoteException;
+}
